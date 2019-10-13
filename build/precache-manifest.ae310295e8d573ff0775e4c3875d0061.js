0ce7a67503f5941234a0c814531863e8942822ab
Shreya Shah <sshah0640@gmail.com>
2019-10-14 00:08:35 +1100

    add build directory

---
+++ build/precache-manifest.ae310295e8d573ff0775e4c3875d0061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b214c7c2ef508bbc710eea75c2d0f830",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaf5f6d511e57be1f9e2",
+    "url": "/static/css/main.791b11b6.chunk.css"
+  },
+  {
+    "revision": "0f5206c40f672fad7bba",
+    "url": "/static/js/2.487e1f98.chunk.js"
+  },
+  {
+    "revision": "eaf5f6d511e57be1f9e2",
+    "url": "/static/js/main.cbdbd108.chunk.js"
+  },
+  {
+    "revision": "76cd165b743657a372a9",
+    "url": "/static/js/runtime-main.a613037e.js"
+  },
+  {
+    "revision": "f7fa8fd0b80a9de96c4a78e8f14b211f",
+    "url": "/static/media/contact.f7fa8fd0.png"
+  }
+]);
